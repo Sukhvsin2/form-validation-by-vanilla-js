16dff48227020a20e23cbcf10ed1764b1fbc9a1a
Sukhvsin2 <46042060+Sukhvsin2@users.noreply.github.com>
2020-08-20 15:19:24 +0530

    on from submit added

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ document.getElementById("app").innerHTML = `
       </div>
       
       <div class="form-control">
-        Address:
+        Address:<br/>
         <textarea id="address"></textarea><br/>
         <small></small>
       </div>
@@ -55,6 +55,7 @@ form.addEventListener("submit", (e) => {
 });
 
 function checkInputs() {
+  let toss = 1;
   const user = username.value.trim();
   const pas1 = password.value.trim();
   const pas2 = password2.value.trim();
@@ -63,42 +64,64 @@ function checkInputs() {
   const addr = address.value;
   if (user === "") {
     setError(username, "Username can't be black!");
+    toss = 0;
   } else {
     setClass(username);
+    toss = 1;
   }
 
   if (em === "") {
     setError(email, "email can't be black!");
+    toss = 0;
   } else {
     setClass(email);
+    toss = 1;
   }
 
   if (pas1 === "") {
     setError(password, "Can't go without password!!");
+    toss = 0;
   } else {
     setClass(password);
+    toss = 1;
   }
 
   if (pas2 === "") {
     setError(password2, "Can't go without password!!");
+    toss = 0;
   } else if (pas1 !== pas2) {
     setError(pas2, "Passwords doesn't match!");
+    toss = 0;
   } else {
     setClass(password2);
+    toss = 1;
   }
 
   if (cont === "") {
     setError(contact, "Please enter your phone number");
+    toss = 0;
   } else if (cont.length < 10) {
     setError(contact, "Contact is not valid!");
+    toss = 0;
   } else {
     setClass(contact);
+    toss = 1;
   }
 
   if (addr === "") {
-    setError(address, "fill your address please.");
+    setError(address, "Address is must.");
+    toss = 0;
   } else {
     setClass(address);
+    toss = 1;
+  }
+
+  // success
+  if (toss) {
+    // window.location.href = "https://giphy.com/gifs/gilmoregirls-netflix-gilmore-girls-xUySTJTRyiGgMGduSY/fullscreen";
+    const gif = document.getElementById("app");
+    gif.innerHTML =
+      '<div id="gif" style="width:100%;height:0;margin-top:-300px;"><iframe src="https://giphy.com/embed/llsgth6glyrYzJ1Ik3" width="500px" height="500px" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/recordingacademy-grammys-2020-llsgth6glyrYzJ1Ik3"></a></p>';
   }
 }
 
